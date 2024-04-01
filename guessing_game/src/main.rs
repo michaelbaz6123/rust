a3483f045acd7cab1057ed7469e99af437a42305
michael.baz <michael.baz@servicenow.com>
2024-04-01 15:10:51 -0400

    Guessing game complete

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,42 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess a number!");
-    println!("Please input your guess.");
+    println!("Guess the number!");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-	.read_line(&mut guess)
-	.expect("Failed to read line");
+    // println!("The secret number is: {secret_number}");
 
-    println!("You guessed: {guess}");
-    
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");     // read_line returns Result: Err or Ok
+
+        // shadow the old guess var, trim newline and parse to u32
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        // Note that this would not work without u32 specified, as we need to use
+        // the right "parse" function that returns the right type
+        println!("You guessed: {guess}");
+
+
+        // match expression is made up of "arms"
+        match guess.cmp(&secret_number) {
+            // each arm has a "pattern"
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
